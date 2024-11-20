b3ce2f717a0a85595ec8e20e6298b7c2cd02bd4d
정유진 <junggg097@daum.net>
2024-11-20 15:39:24 +0900

    refactor : REST API concept -> concepts 복수형 변경

--- src/main/java/com/toucheese/conceptstudio/controller/ConceptStudioController.java
+++ src/main/java/com/toucheese/conceptstudio/controller/ConceptStudioController.java
@@ -8,14 +8,12 @@ import com.toucheese.studio.entity.Studio;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/concept")
+@RequestMapping("/api/v1/concepts")
 public class ConceptStudioController {
     private final ConceptStudioService conceptStudioService;
 
