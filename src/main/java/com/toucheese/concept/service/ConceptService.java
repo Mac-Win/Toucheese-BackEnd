3f7cd08586344d628990b7cdaafc4ee5d5217427
ppusda <ppusda1234@gmail.com>
2024-11-29 13:41:29 +0900

    refactor: DTO => Response 네이밍 변경

--- src/main/java/com/toucheese/concept/service/ConceptService.java
+++ src/main/java/com/toucheese/concept/service/ConceptService.java
@@ -1,6 +1,6 @@
 package com.toucheese.concept.service;
 
-import com.toucheese.concept.dto.ConceptDTO;
+import com.toucheese.concept.dto.ConceptResponse;
 import com.toucheese.concept.entity.Concept;
 import com.toucheese.concept.repository.ConceptRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +17,10 @@ public class ConceptService {
     private final ConceptRepository conceptRepository;
 
     @Transactional(readOnly = true)
-    public List<ConceptDTO> getAllConcepts() {
+    public List<ConceptResponse> getAllConcepts() {
         List<Concept> concepts = conceptRepository.findAll();
         return concepts.stream()
-                .map(ConceptDTO::of)
-                .collect(Collectors.toList());
+                .map(ConceptResponse::of)
+                .toList();
     }
 }
