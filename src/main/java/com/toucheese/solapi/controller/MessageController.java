0d1e6c57f1b8a5f9cc388bc5cb1e1dd28aed22a2
정유진 <junggg097@daum.net>
2024-12-04 15:33:43 +0900

    feat : Message 발송 기본 로직 구현

--- src/main/java/com/toucheese/solapi/controller/MessageController.java
+++ src/main/java/com/toucheese/solapi/controller/MessageController.java
@@ -1,4 +1,35 @@
 package com.toucheese.solapi.controller;
 
+import com.toucheese.solapi.dto.MessageRequest;
+import com.toucheese.solapi.service.MessageService;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/messages")
 public class MessageController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestBody MessageRequest messageRequest) {
+        try {
+            String result = messageService.sendMessage(messageRequest);
+            return ResponseEntity.ok(result);
+        } catch (NurigoMessageNotReceivedException exception) {
+            return ResponseEntity.status(500)
+                    .body("Failed to send some messages: " + exception.getFailedMessageList());
+        } catch (Exception exception) {
+            return ResponseEntity.status(500).body("Error occurred: "  + exception.getMessage());
+        }
+    }
 }
--- src/main/java/com/toucheese/solapi/service/MessageService.java
+++ src/main/java/com/toucheese/solapi/service/MessageService.java
@@ -1,4 +1,31 @@
 package com.toucheese.solapi.service;
 
+import com.toucheese.solapi.dto.MessageRequest;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
+    private final DefaultMessageService solapiService;
+
+    public MessageService() {
+        this.solapiService = NurigoApp.INSTANCE.initialize(
+                "NCSE0CV4QQKN79LE",
+                "80GTLY3XHFNNTCTRGPSORX5UZFUM9GJJ",
+                "https://api.solapi.com"
+
+        );
+    }
+
+    public String sendMessage(MessageRequest request) throws Exception {
+        Message message = new Message();
+        message.setFrom(request.from());
+        message.setTo(request.recipient());
+        message.setText(request.text());
+
+        solapiService.send(message);
+        return "Message sent successfully.";
+    }
 }
