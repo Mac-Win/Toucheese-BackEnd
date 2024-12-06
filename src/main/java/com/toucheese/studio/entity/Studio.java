9545d2863710959cb1cafde2cba77797595ea437
전화철 <lunaris0206@naver.com>
2024-12-06 10:01:54 +0900

    feat : 스튜디오 상세조회 영업시간 기능 구현
    스튜디오 상세조회 영업시간이 String이었던 것을 List로 반환합니다.
    List로 반환하여 유동적인 영업시간이 가능합니다.
    추가로 ReservationController의 Swagger 문서 수정하였습니다.

--- src/main/java/com/toucheese/studio/entity/Studio.java
+++ src/main/java/com/toucheese/studio/entity/Studio.java
@@ -1,10 +1,13 @@
 package com.toucheese.studio.entity;
 
+import java.util.List;
+
 import com.toucheese.conceptstudio.entity.ConceptStudio;
 import com.toucheese.image.entity.FacilityImage;
 import com.toucheese.image.entity.StudioImage;
 import com.toucheese.product.entity.Product;
 import com.toucheese.review.entity.Review;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +19,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,7 +43,9 @@ public class Studio {
 
     private Float rating;
 
-    private String operationHour;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "studio_id")
+    private List<OperatingHour> operatingHours;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -62,8 +66,7 @@ public class Studio {
     @OneToMany(mappedBy = "studio", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<ConceptStudio> conceptStudios;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "studio_id")
+    @OneToMany(mappedBy = "studio", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Product> products;
 
     @OneToMany(mappedBy = "studio", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
