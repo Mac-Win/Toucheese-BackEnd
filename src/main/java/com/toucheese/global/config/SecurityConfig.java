348df2b12f005e505f00ae66aa1d8f79dad0e445
ppusda <ppusda1234@gmail.com>
2024-12-08 03:50:38 +0900

    refactor: addAllowedOrigin에 개발환경 추가

--- src/main/java/com/toucheese/global/config/SecurityConfig.java
+++ src/main/java/com/toucheese/global/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.addAllowedOrigin("https://www.toucheese-macwin.store");
+		configuration.addAllowedOrigin("http://localhost:3000");
 		configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
 		configuration.addAllowedHeader("*"); // 모든 헤더 허용
 		configuration.setAllowCredentials(true); // 인증 정보 허용
