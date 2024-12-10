76a43b68965146f234ebdd9c4f4f809fe07aa87f
전화철 <lunaris0206@naver.com>
2024-12-10 13:41:33 +0900

    feat : 회원 정보 기능 구현

--- src/main/java/com/toucheese/member/controller/MemberController.java
+++ src/main/java/com/toucheese/member/controller/MemberController.java
@@ -1,19 +1,25 @@
 package com.toucheese.member.controller;
 
-import com.toucheese.global.data.ApiResponse;
-import com.toucheese.member.dto.LoginMemberResponse;
-import com.toucheese.member.dto.LoginRequest;
-import com.toucheese.member.dto.LoginResponse;
-import com.toucheese.member.service.MemberService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import java.security.Principal;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.toucheese.global.data.ApiResponse;
+import com.toucheese.member.dto.LoginMemberResponse;
+import com.toucheese.member.dto.LoginRequest;
+import com.toucheese.member.dto.LoginResponse;
+import com.toucheese.member.dto.MemberContactInfoResponse;
+import com.toucheese.member.service.MemberService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,6 +41,15 @@ public class MemberController {
         return ApiResponse.accessTokenResponse(
             LoginResponse.of(loginMemberResponse), loginMemberResponse.accessToken()
         );
+    }
+
+    @GetMapping("/infos")
+    @Operation(summary = "회원 정보", description = "회원 이름, 이메일, 전화번호의 정보를 얻습니다.")
+    public MemberContactInfoResponse findMemberContactInfo(Principal principal) {
+
+        Long memberId = Long.parseLong(principal.getName());
+        ;
+        return memberService.findMemberContactInfo(memberId);
     }
 
 }
