0d63893166d77f615b210567e74b627c2dd2c77b
ppusda <ppusda1234@gmail.com>
2024-12-13 14:57:40 +0900

    feat: 새 이미지 업로드 기능 추가

--- src/main/java/com/toucheese/image/controller/ImageController.java
+++ src/main/java/com/toucheese/image/controller/ImageController.java
@@ -3,25 +3,40 @@ package com.toucheese.image.controller;
 import com.toucheese.image.service.ImageService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/images")
 public class ImageController {
 
     private final ImageService imageService;
 
     /**
-     * Stream 방식으로 이미지 업로드
+     * Stream 방식으로 이미지 업로드 (기존 이미지 업데이트용)
      * @param request 요청 정보 (InputStream, Metadata)
      * @param filename 파일이름
      */
-    @PostMapping
-    public void streamImageUpload(
+    @PostMapping("/v1/images")
+    public void streamExistingImageUpload(
             HttpServletRequest request,
             @RequestParam String filename
     ) {
-        imageService.streamImageUpload(request, filename);
+        imageService.uploadExistingImage(request, filename);
+    }
+
+    /**
+     * Stream 방식으로 이미지 업로드 (새 이미지 업로드용)
+     * @param request 요청 정보 (InputStream, Metadata)
+     * @param filename 파일이름
+     */
+    @PostMapping("/v2/images")
+    public void streamImageUpload(
+            HttpServletRequest request,
+            @RequestParam String filename,
+            @RequestParam Long studioId
+    ) {
+        imageService.uploadNewImage(request, filename, studioId);
     }
 }
