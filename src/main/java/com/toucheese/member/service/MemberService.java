d64be4cc05f7ab83a6a92a9554ce8a742b8db731
전화철 <lunaris0206@naver.com>
2024-12-29 21:37:28 +0900

    feat : 일반 회원가입 기능 구현

--- src/main/java/com/toucheese/member/service/MemberService.java
+++ src/main/java/com/toucheese/member/service/MemberService.java
@@ -13,99 +13,110 @@ import com.toucheese.member.dto.LoginRequest;
 import com.toucheese.member.dto.MemberContactInfoResponse;
 import com.toucheese.member.dto.MemberFirstLoginUpdateRequest;
 import com.toucheese.member.dto.MemberTokenResponse;
+import com.toucheese.member.dto.SignupRequest;
 import com.toucheese.member.dto.TokenDTO;
 import com.toucheese.member.entity.Member;
 import com.toucheese.member.entity.Role;
 import com.toucheese.member.repository.MemberRepository;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberRepository memberRepository;
-    private final TokenService tokenService;
+	private final MemberRepository memberRepository;
+	private final TokenService tokenService;
 
-    /**
-     * 회원 정보 검색
-     * @param id 회원 아이디
-     * @return 회원 정보
-     */
-    @Transactional(readOnly = true)
-    public Member findMemberById(Long id) {
-        return memberRepository.findById(id)
-                .orElseThrow(ToucheeseBadRequestException::new);
-    }
+	/**
+	 * 회원 정보 검색
+	 * @param id 회원 아이디
+	 * @return 회원 정보
+	 */
+	@Transactional(readOnly = true)
+	public Member findMemberById(Long id) {
+		return memberRepository.findById(id)
+			.orElseThrow(ToucheeseBadRequestException::new);
+	}
 
-    /**
-     * 로그인 처리를 위한 메서드
-     * @param loginRequest 로그인 정보 (email, password, deviceId)
-     * @return 로그인 시 생성 된 접근 토큰
-     */
-    @Transactional
-    public MemberTokenResponse login(LoginRequest loginRequest) {
-        Member member = memberRepository.findByEmail(loginRequest.email())
-                .orElseThrow(() -> new ToucheeseBadRequestException("아이디 혹은 비밀번호가 잘못되었습니다."));
+	/**
+	 * 로그인 처리를 위한 메서드
+	 * @param loginRequest 로그인 정보 (email, password, deviceId)
+	 * @return 로그인 시 생성 된 접근 토큰
+	 */
+	@Transactional
+	public MemberTokenResponse login(LoginRequest loginRequest) {
+		Member member = memberRepository.findByEmail(loginRequest.email())
+			.orElseThrow(() -> new ToucheeseBadRequestException("아이디 혹은 비밀번호가 잘못되었습니다."));
 
-        checkMemberPassword(member, loginRequest.password());
-        TokenDTO tokenDTO = tokenService.loginMemberToken(member, loginRequest.deviceId());
+		checkMemberPassword(member, loginRequest.password());
+		TokenDTO tokenDTO = tokenService.loginMemberToken(member, loginRequest.deviceId());
 
-        return MemberTokenResponse.builder()
-                .memberId(member.getId())
-                .name(member.getName())
-                .tokenDTO(tokenDTO)
-                .build();
-    }
+		return MemberTokenResponse.builder()
+			.memberId(member.getId())
+			.name(member.getName())
+			.tokenDTO(tokenDTO)
+			.build();
+	}
 
-    /**
-     * 비밀번호가 같은지 확인하기 위한 메서드
-     * @param member email로 찾은 회원
-     * @param password 입력한 비밀번호
-     */
-    private void checkMemberPassword(Member member, String password) {
-        if (!member.getPassword().equals(password)) {
-            throw new ToucheeseBadRequestException("아이디 혹은 비밀번호가 잘못되었습니다.");
-        }
-    }
+	/**
+	 * 비밀번호가 같은지 확인하기 위한 메서드
+	 * @param member email로 찾은 회원
+	 * @param password 입력한 비밀번호
+	 */
+	private void checkMemberPassword(Member member, String password) {
+		if (!member.getPassword().equals(password)) {
+			throw new ToucheeseBadRequestException("아이디 혹은 비밀번호가 잘못되었습니다.");
+		}
+	}
 
-    @Transactional(readOnly = true)
-    public MemberContactInfoResponse findMemberContactInfo(Long memberId) {
-        return MemberContactInfoResponse.of(findMemberById(memberId));
-    }
+	@Transactional(readOnly = true)
+	public MemberContactInfoResponse findMemberContactInfo(Long memberId) {
+		return MemberContactInfoResponse.of(findMemberById(memberId));
+	}
 
+	/**
+	 * 카카오 사용자 정보를 기반으로 회원 조회 또는 생성
+	 * @param kakaoMember 카카오 사용자 정보
+	 * @return 회원 엔티티
+	 */
+	public Member findOrCreateMember(KakaoMember kakaoMember) {
+		return memberRepository.findByEmail(kakaoMember.email())
+			.orElseGet(() -> createMember(kakaoMember));
+	}
 
+	private Member createMember(KakaoMember kakaoMember) {
+		Member member = Member.builder()
+			.email(kakaoMember.email())
+			.name(kakaoMember.nickname())
+			.password(null) // 소셜 로그인 사용자는 비밀번호 없음
+			.role(Role.USER)
+			.authProvider(AuthProvider.KAKAO)
+			.isFirstLogin(true) // 처음 생성되는 경우 true로 설정
+			.build();
+		return memberRepository.save(member);
+	}
 
+	@Transactional
+	public void memberFirstLoginUpdate(MemberFirstLoginUpdateRequest request, Principal principal) {
+		Long MemberId = PrincipalUtils.extractMemberId(principal);
 
+		Member member = findMemberById(MemberId);
 
-    /**
-     * 카카오 사용자 정보를 기반으로 회원 조회 또는 생성
-     * @param kakaoMember 카카오 사용자 정보
-     * @return 회원 엔티티
-     */
-    public Member findOrCreateMember(KakaoMember kakaoMember) {
-        return memberRepository.findByEmail(kakaoMember.email())
-            .orElseGet(() -> createMember(kakaoMember));
-    }
+		member.firstLoginUpdate(request);
+	}
 
-    private Member createMember(KakaoMember kakaoMember) {
-        Member member = Member.builder()
-            .email(kakaoMember.email())
-            .name(kakaoMember.nickname())
-            .password(null) // 소셜 로그인 사용자는 비밀번호 없음
-            .role(Role.USER)
-            .authProvider(AuthProvider.KAKAO)
-            .isFirstLogin(true) // 처음 생성되는 경우 true로 설정
-            .build();
-        return memberRepository.save(member);
-    }
-
-    @Transactional
-    public void memberFirstLoginUpdate(MemberFirstLoginUpdateRequest request, Principal principal) {
-        Long MemberId = PrincipalUtils.extractMemberId(principal);
-
-        Member member = findMemberById(MemberId);
-
-        member.firstLoginUpdate(request);
-    }
+	@Transactional
+	public void createSignup(@Valid SignupRequest signupRequest) {
+		Member member = Member.builder()
+			.email(signupRequest.email())
+			.name(signupRequest.name())
+			.password(signupRequest.password())
+			.phone(signupRequest.phone().replaceAll("(\\d{3})(\\d{3,4})(\\d{4})", "$1-$2-$3"))
+			.role(Role.USER)
+			.authProvider(AuthProvider.LOCAL)
+			.build();
+		memberRepository.save(member);
+	}
 }
