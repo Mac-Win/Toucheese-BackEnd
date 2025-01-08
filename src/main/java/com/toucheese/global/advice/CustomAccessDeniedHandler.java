4ebd54dad6c947189dd31d59bd64f54f3a68f89a
ppusda <ppusda1234@gmail.com>
2025-01-08 20:27:16 +0900

    refactor: 인증 예외 반환 시 캐싱 방지 설정

--- src/main/java/com/toucheese/global/advice/CustomAccessDeniedHandler.java
+++ src/main/java/com/toucheese/global/advice/CustomAccessDeniedHandler.java
@@ -12,8 +12,14 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
             org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException {
+
         // 상태 코드 설정
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        // 캐싱 방지 헤더 추가
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
 
         // 메시지 반환
         response.setContentType("application/json");
